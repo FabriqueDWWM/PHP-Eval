c66e9db17636bdd3fd0e4a70e9d0b4c17a090719
FabriqueDWWM <71813538+FabriqueDWWM@users.noreply.github.com>
2021-03-04 08:54:51 +0100

    Add files via upload

---
+++ blog/src/template/base.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<?php
+include_once TEMPLATE_PARTS . '/head.php';
+?>
+
+<body>
+    <?php
+
+    include_once PAGES . '/' . $template;
+
+    ?>
+
+    <script src="<?= SCRIPTS_PATH ?>/main.js"></script>
+</body>
+
+</html>
