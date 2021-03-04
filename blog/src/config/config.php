e25b309bcbb9ba8188394bec6f7a2f47426c168c
FabriqueDWWM <71813538+FabriqueDWWM@users.noreply.github.com>
2021-03-04 08:54:15 +0100

    Add files via upload

---
+++ blog/src/config/config.php
@@ -0,0 +1,40 @@
+<?php
+
+define('INDEX_PATH', 'PUBLIC');
+
+define('ROOT', "");
+
+define('SRC', ROOT . "src");
+
+define('PUB', "public");
+
+define('ASSETS_PATH', PUB . "/assets");
+
+define('STYLESHEET_DIR_PATH', ASSETS_PATH . "/css");
+
+define('IMAGES_PATH', ASSETS_PATH . "/images");
+
+define("SCRIPTS_PATH", ASSETS_PATH . "/js");
+
+define('TEMPLATE', SRC . "/template");
+
+define('TEMPLATE_PARTS', TEMPLATE . '/template-parts');
+
+define('PAGES', TEMPLATE . '/pages');
+
+define('ROUTES', include SRC . '/config/routes.php');
+
+define('DATAS_PATH', SRC . '/datas');
+
+define('CLASSES', SRC . "/classes");
+
+//personnalisation
+
+define('TITLE', 'Blog');
+
+// BDD
+
+define('HOST', "localhost");
+define("SQLUSER", "root");
+define("SQLPWD", "");
+define("DBNAME", "blog");
---
+++ blog/src/config/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+return array(
+    "accueil" => "accueil",
+    "page404" => "page404",
+);
